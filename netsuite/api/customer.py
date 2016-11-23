9596373b556d5895d01ce8fef13fa15ce2e82ae9
Frantisek Malina <fmalina@gmail.com>
2016-11-23 17:09:09 +0000

    Fixed customer lookup

--- netsuite/api/customer.py
+++ netsuite/api/customer.py
@@ -3,7 +3,7 @@ Add a customer, lookup customer if adding fails with UNIQUE_CUST_ID_REQD.
 Proceed to CashSale.
 """
 
-from netsuite.client import client
+from netsuite.client import client, passport, app_info
 from netsuite.test_data import data
 from netsuite.utils import get_record_by_type
 from netsuite.service import (Customer,
@@ -41,15 +41,17 @@ def get_customer(internal_id):
 def lookup_customer(customer_data):
     d = {}
     for k, v in customer_data.items():
-        d[k] = SearchStringField(searchValue=v,
-                                 operator='is')
+        if k == 'phone':
+            continue
+        d[k] = SearchStringField(searchValue=v, operator='is')
+
     customer_search = CustomerSearchBasic(**d)
 
     search_preferences = SearchPreferences(bodyFieldsOnly=False,
                                            returnSearchColumns=True,
                                            pageSize=20)
 
-    response = client.service.search(searchRecord=item_search, _soapheaders={
+    response = client.service.search(searchRecord=customer_search, _soapheaders={
         'searchPreferences': search_preferences,
         'applicationInfo': app_info,
         'passport': passport,
