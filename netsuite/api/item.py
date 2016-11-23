af657da1e75144b5dd02441c401085771563838a
Oludolapo Soboyede <dolimoon6000@yahoo.com>
2016-11-23 16:52:12 +0000

    Updated item module to provide functionality for listing products
    That is, it fetches details for a list of products

--- netsuite/api/item.py
+++ netsuite/api/item.py
@@ -3,27 +3,25 @@ Product search
 """
 from netsuite.client import client, passport, app_info
 from netsuite.service import (ItemSearchBasic,
-                              SearchBooleanField,
-                              SearchPreferences)
-from lxml import etree
+                              SearchPreferences,
+                              SearchMultiSelectField,
+                              RecordRef)
 
 
-def find_products():
-    item_search = ItemSearchBasic(isInactive=SearchBooleanField(searchValue=True))
+def list_products(internal_ids):
+    id_references = [RecordRef(internalId=id) for id in internal_ids]
+    item_search = ItemSearchBasic(internalId=SearchMultiSelectField(searchValue=id_references,
+                                                                    operator='anyOf'
+                                                                    ))
     search_preferences = SearchPreferences(bodyFieldsOnly=False,
                                            returnSearchColumns=True,
                                            pageSize=20)
-    print('raw xml')
-    print(etree.tostring(client.service._binding.create_message('search', item_search, _soapheaders={
-        'searchPreferences': search_preferences,
-        'applicationInfo': app_info,
-        'passport': passport
-    })))
 
     result = client.service.search(searchRecord=item_search, _soapheaders={
         'searchPreferences': search_preferences,
         'applicationInfo': app_info,
         'passport': passport,
     })
-    print('result:')
-    print(result)
+
+    if result['body']['searchResult']['status']['isSuccess']:
+        return result['body']['searchResult']['recordList']['record']
